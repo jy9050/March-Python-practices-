0f671de3163ac065a044b5a065251b17cac2ec1a
Jyoti Kashyap <kashyapjyoti082000@gmail.com>
2026-03-16 22:34:07 +0530

    March Day 1

---
+++ March Day 1.py
@@ -0,0 +1,17 @@
+# print("I am learning Data Science")
+# my_age = 26
+# my_city = "Panchkula"
+# print("my_age:",my_age)
+# print("my_city:",my_city)
+
+# # f-string   This format is used a lot in data science scripts.
+# print(f"my_age is {my_age}")
+# print(f"my_city is {my_city}")
+
+my_name = "Jyoti"
+course = "Data science"
+print("My name is :",my_name)
+print("I am learning :", course)
+
+print(f"My name is {my_name}")
+print(f"I am learning {course}")
