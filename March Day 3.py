cfd2017fc63dffe413a5058eba4d36fbb0e56a9e
Jyoti Kashyap <kashyapjyoti082000@gmail.com>
2026-03-19 21:05:05 +0530

    March Day 3

---
+++ March Day 3.py
@@ -0,0 +1,24 @@
+# Emp_package = 30000
+# if Emp_package > 10000:
+#     print("Employee Don't Resign")
+# else:
+#     print("Employee Resign")
+    
+# salary = 45000
+# if salary > 40000:
+#     print("High income group")
+# else:
+#     print("Low income group")    
+    
+
+
+# Elif condition
+
+company_budget = 50000
+if company_budget >=500000:
+    print("Permote")
+elif company_budget >=5000:
+    print("Not permote")
+else:
+    print("fire")
+        
