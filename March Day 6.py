ac7f50f61483c656f6c3d6ed708b930084acff40
Jyoti Kashyap <kashyapjyoti082000@gmail.com>
2026-03-22 14:37:33 +0530

    March Day 6

---
+++ March Day 6.py
@@ -0,0 +1,14 @@
+# for i in range(1,2):
+#     print(i*2)
+    
+# for i in range(3):
+    # print(i+2)    
+    
+# for i in range(4):
+#     print(i*3)    
+
+# for i in range(1,4):
+    # print(i+1)
+    
+for i in range(3):
+    print(i*i)    
