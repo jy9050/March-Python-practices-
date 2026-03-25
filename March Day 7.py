d9f2f1561481594083a9eca7bd3f28022ef06d84
Jyoti Kashyap <kashyapjyoti082000@gmail.com>
2026-03-25 21:59:59 +0530

    March Day 7

---
+++ March Day 7.py
@@ -0,0 +1,31 @@
+# number = [10,20,30]
+# for num in number:
+#     print(num)
+    
+    
+# numbers = [2,4,6,8]
+# for num in numbers:
+    # print(num*2)    
+    
+    
+# number = [2,8,10,12,14]
+# number.append(50)
+# print(number)    
+
+
+# number = [3,6,8,10]
+# print(len(number))
+
+# numbers = [10,20,30,40,50]
+# total = 0
+# for num in numbers:
+#     total= total+num
+#     print(total)
+    
+# number = [2,8,10,12,14]
+# number.pop()
+# print(number)      
+
+
+number = [2,8,10,12,14]
+print(max(number))  
