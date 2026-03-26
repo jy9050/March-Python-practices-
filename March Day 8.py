29f8f9cf236c7d446552a55022372818b4b0e7dd
Jyoti Kashyap <kashyapjyoti082000@gmail.com>
2026-03-26 21:39:51 +0530

    March Day 8

---
+++ March Day 8.py
@@ -0,0 +1,38 @@
+# num = 15
+# if num > 10:
+#     print("Big")
+# else:
+#     print("Small")
+
+# num = 2
+# if num % 2 ==0:
+#     print("even")
+# else:
+#     print("odd")
+    
+    
+# num = 15
+# if num >20:
+#     print("A")
+# elif  num >10:
+#     print("B")
+# else:
+#     print("C")    
+
+
+# num = 3
+# if num == 1:
+#     print("one")
+# elif num == 2:
+#     print("two")
+# else:
+#     print("other")
+    
+num = 0
+if num >0:
+    print("Positive")
+elif num ==0:
+    print("Zero")
+else:
+    print("Negative")
+        
