46159e645ea2cbcaa9800abf9fba66b5f6891c28
Jyoti Kashyap <kashyapjyoti082000@gmail.com>
2026-03-27 22:40:05 +0530

    March Day9

---
+++ March Day9.py
@@ -0,0 +1,26 @@
+# marks =75
+# if marks >=90:
+#     print("Grade A")
+# elif marks >=70:
+#     print("Garde B")
+# elif marks >=50:
+#     print("Grade C")
+# else:
+#     print("Fail")
+    
+# num = 7
+# if num % 2 != 0:
+#     if num >0:
+#         print("Positive odd") 
+#     else:
+#         print("Negative odd")
+
+password = "90507010"
+
+
+if password == "90507010":
+    print("Access Granted")
+else:
+    print("Access Denied")
+        
+           
