997ebfa85be80749c85155c167eddd3059a990c5
Jyoti Kashyap <kashyapjyoti082000@gmail.com>
2026-03-28 22:11:03 +0530

    March Day10

---
+++ March Day10.py
@@ -0,0 +1,17 @@
+# Simple Calculator
+num1 = 10
+num2 = 50
+op = "-"
+
+if op == "+":
+    print(num1 + num2)
+elif op == "-":
+    print(num1 - num2)
+elif op == "*":
+    print(num1 * num2)
+elif op == "/":
+    print(num1 / num2)
+elif op == "%":
+    print(num1 % num2)
+else:
+    print("Invalid Operator")
